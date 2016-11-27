037d5e5b95727a6d8617f280481ec7ba13449a94
Lindsey Ullman <lindsey.ullman@gmail.com>
2016-11-26 16:49:32 -0800

    Add session support, logon form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'root#index'
 
   resources :users
-  
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  get 'logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
