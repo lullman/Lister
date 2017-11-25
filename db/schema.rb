a5e4e4aaa11eccacac32abd303b2eb8aa79f9bb4
Lindsey Ullman <lindsey.ullman@gmail.com>
2017-11-24 18:20:52 -0700

    Rename users table to users_old

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304195209) do
+ActiveRecord::Schema.define(version: 20171125011636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20170304195209) do
     t.boolean "public", default: true
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users_old", force: :cascade do |t|
     t.string "username", null: false
     t.string "password_hash", null: false
     t.string "first_name"
