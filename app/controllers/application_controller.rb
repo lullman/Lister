2fbd35c0c8c3601062ee8853ee0468d1b25a6c59
Lindsey Ullman <lindsey.ullman@gmail.com>
2017-12-01 18:31:27 -0700

    Add first_name and last_name parameters to permit list for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # before_action :require_login
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   include SessionsHelper
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
 
   private
 
